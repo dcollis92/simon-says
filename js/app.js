98c1cff04997fc2715f0930d2a0b22ed7627e983
dcollis92 <davidphilipcollis@gmail.com>
2022-02-11 11:43:49 -0600

    CSS edit

--- js/app.js
+++ js/app.js
@@ -1,19 +1,21 @@
 /*-------------------------- Constants --------------------------*/
-player = {
-
+player = { // PC 0.1
+  name: 'Ready Player One',
+  score: 0
 }
 
-buttons = ['red', 'green', 'yellow', 'blue']
+buttons = ['red', 'green', 'yellow', 'blue'] // PC 0.2
 
 
 /*-------------------------- Variables --------------------------*/
 let level, isWinner, 
-
+// PC 1.2     1.3
 
 
 /*------------------ Cached Element References ------------------*/
-sequenceArray = document.querySelectorAll('.board-btn')
-gameStatus = document.querySelector('#message')
+sequenceArray = document.querySelectorAll('.board-btn') // PC 2.1
+gameStatus = document.querySelector('#message') // PC 2.2
+startButton = document.querySelector('#start-level') // PC 3.2
 const resetDiv = document.getElementById('reset-div')
 const resetBtn = document.querySelector('#reset-button')
 
@@ -28,6 +30,10 @@ resetBtn.addEventListener('click', init)
 init()
 
 function init() {
+  let gameSequence = [''] // 3.2
+  let playerSequence = [''] // 3.2
+  level = 1 // PC 3.2
+  isWinner = null // PC 3.2
 
 
   render()
