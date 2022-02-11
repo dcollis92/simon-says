3fa335172d4c56dd98ef64e08c0b8f952f1f0719
dcollis92 <davidphilipcollis@gmail.com>
2022-02-11 08:11:07 -0600

    stub up functions and add variables

--- js/app.js
+++ js/app.js
@@ -1,20 +1,44 @@
 /*-------------------------- Constants --------------------------*/
+player = {
 
+}
+
+buttons = ['red', 'green', 'yellow', 'blue']
 
 
 /*-------------------------- Variables --------------------------*/
-
+let level, isWinner, 
 
 
 
 /*------------------ Cached Element References ------------------*/
-
-
+sequenceArray = document.querySelectorAll('.board-btn')
+gameStatus = document.querySelector('#message')
+const resetDiv = document.getElementById('reset-div')
+const resetBtn = document.querySelector('#reset-button')
 
 
 /*----------------------- Event Listeners -----------------------*/
 
-
+resetBtn.addEventListener('click', init)
 
 
 /*-------------------------- Functions --------------------------*/
+init()
+
+function init() {
+
+}
+
+function render() {
+
+}
+
+function handleClick() {
+
+}
+
+function getWinner() {
+
+}
+
