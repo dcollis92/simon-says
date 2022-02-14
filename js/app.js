b24257012e5722c9260f1557db05bf96f73cc67a
dcollis92 <davidphilipcollis@gmail.com>
2022-02-14 11:01:41 -0600

    start audio implimentation

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 /*-------------------------- Constants --------------------------*/
 const player = {
-  // PC 0.1
   name: "Human",
-  score: 0,
+  score = level -1
 };
 
-const buttons = ["green", "red", "yellow", "blue"]; // PC 0.2
+const buttons = ["green", "red", "yellow", "blue"];
 
 /*-------------------------- Variables --------------------------*/
 let level,
@@ -13,19 +12,28 @@ let level,
   isWinner,
   //  PC 1.3
   turn,
-  /*------------------ Cached Element References ------------------*/
-  sequenceArray = document.querySelectorAll(".board-btn"); // PC 2.1
+
+/*------------------ Cached Element References ------------------*/
+sequenceArray = document.querySelectorAll(".board-btn"); // PC 2.1
 gameStatus = document.querySelector("#message"); // PC 2.2
 startButton = document.querySelector("#start-level"); // PC 3.2
-const resetDiv = document.getElementById("reset-div");
-const resetBtn = document.querySelector("#reset-button");
+resetDiv = document.getElementById("reset-div");
+resetBtn = document.querySelector("#reset-button");
+btnAudio = document.querySelector(".btn-style")
 
 /*----------------------- Event Listeners -----------------------*/
 startButton.addEventListener("click", compInput);
 sequenceArray.forEach((button) =>
-  button.addEventListener("click", playerInput)
-);
+  button.addEventListener("click", playerInput));
 resetBtn.addEventListener("click", init);
+
+sequenceArray.addEventListener("click", evt => {
+  if (evt.target.id !== "not-fox"){
+    const audioElement = new Audio(`../audio/${evt.target.id}.mp3`)
+    audioElement.volume = .01
+    audioElement.play()
+  }
+})
 
 /*-------------------------- Functions --------------------------*/
 init(); // PC 3.1
@@ -49,34 +57,35 @@ function render() {
 }
 
 function compRender() {
-  buttons.
-  setTimeout(() => {buttons}, 1000);
+  sequenceArray.classList
+  setTimeout(() => {}, 1000);
   render()
 }
 
 function playerRender() {
-
+  
   render()
 }
+
+// create separate render functions for the comp and player
+// each render should add a class assigning color+note
+// then a setTimeout to run through the whole array, checking 1 by one what each item is, delaying by a second each.
+// then it should remove the class to turn off audio+color
 
 function genSequence() {
   gameSequence.push(buttons[Math.floor(Math.random() * buttons.length)]);
   console.log("gs", gameSequence)
 }
 
-
 function compInput(event) {
   if (turn !== -1) {
     return;
   }
   genSequence()
-  // why isn't this working?
   compRender();
   render();
   turn = 1;
-  console.log("gs", gameSequence);
 }
-console.log("gs", gameSequence);
 
 function playerInput(event) {
   if (turn !== 1) {
@@ -88,35 +97,30 @@ function playerInput(event) {
     // The player's last guess is incorrrect, and they've lost
     // we can return out of this function.
   } else {
-    // The player's last guess is correct, and they can continue
-    // (you may not even need this)
   }
-  console.log(gameSequence)
-  console.log(playerSequence)
+  console.log('gs', gameSequence)
+  console.log('ps', playerSequence)
   if (playerSequence.length === gameSequence.length) {
-    // If we've reached this point then the player's last guess is correct
-    // AND we've reached the end of the current sequence, time for the computer
-    // to show what's next
     level += 1;
     display = level;
-    console.log("ps", playerSequence);
     playerSequence = [];
     turn = -1; // back to computers turn
     playerRender()
     compInput(); // invoke computers turn
   }
+  getWinner()
   render()
 }
 
 function getWinner() {
-  if (gameSequence === playerSequence) {
+  if (playerSequence.length === (gameSequence.length -1)) {
     message = "Nice! Keep Going!";
     // game continues
-  } else if (gameSequence === playerSequence && level > 15) {
+  } else if (playerSequence.length === gameSequence.length && level > 15) {
     isWinner = true;
-    message = `${player.name} Won! Play Again?`;
+    message = `${player.name} Won! You beat all ${score} levels! Play Again?`;
     confetti.start(2000);
-  } else if (gameSequence !== playerSequence) {
+  } else if (playerSequence.length !== (gameSequence.length -1)) {
     message = "D'oh! Try Again?";
   }
   resetDiv.classList.remove("hidden"); // PC 7.3
